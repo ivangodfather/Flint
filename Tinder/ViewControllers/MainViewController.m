6d677f25f13362505b0c398b09298f622c6046d0
John Blanchard <jnblanchard@mac.com>
2014-08-31 19:49:40 -0500

    photo flopping, and upgraded dislike button

--- Tinder/ViewControllers/MainViewController.m
+++ Tinder/ViewControllers/MainViewController.m
@@ -34,8 +34,13 @@
 @property NSMutableArray* willBeMatches;
 @property UIImageView* profileImage;
 @property UIImageView* backgroundImage;
+@property NSMutableArray* arrayOfPhotoDataForeground;
+@property NSMutableArray* arrayOfPhotoDataBackground;
+@property UILabel* foregroundLabel;
+@property UILabel* backgroundLabel;
 @property BOOL firstTime;
 @property BOOL isRotating;
+@property int photoArrayIndex;
 @end
 
 @implementation MainViewController
@@ -47,6 +52,7 @@
     _sidebarButton.action = @selector(revealToggle:);
     self.posibleMatchesArray = [NSMutableArray new];
     self.willBeMatches = [NSMutableArray new];
+    self.photoArrayIndex = 1;
     self.firstTime = YES;
     self.isRotating = YES;
     [self getMatches];
@@ -68,7 +74,6 @@
         [self.willBeMatches addObjectsFromArray:objects];
         NSLog(@"will be match - %@", objects);
         //[self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
-
         PFQuery *query = [PFQuery queryWithClassName:@"PossibleMatch"]; //matches
         [query whereKey:@"fromUser" equalTo:[UserParse currentUser]]; //people you've seen
         PFQuery* userQuery = [UserParse query];
@@ -79,6 +84,7 @@
             NSLog(@"new matches - %@", objects);
             if (self.firstTime) {
                 UserParse* aUser = self.posibleMatchesArray.firstObject;
+                self.arrayOfPhotoDataForeground = [NSMutableArray new];
                 [self.posibleMatchesArray removeObject:aUser];
                 self.currShowingProfile = aUser;
                 self.profileView.tag = profileViewTag;
@@ -88,6 +94,7 @@
                 NSLog(@"top username %@", aUser.username);
                 NSNumber* age = aUser[@"age"];
                 [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                    [self.arrayOfPhotoDataForeground addObject:data];
                     self.profileView = [[UIView alloc] initWithFrame:[self createMatchRect]];
                     self.profileView.backgroundColor = RED_COLOR;
                     self.profileView.clipsToBounds = YES;
@@ -99,17 +106,41 @@
                     self.profileImage.clipsToBounds = YES;
                     self.profileImage.layer.cornerRadius = cornRadius;
                     [self.profileView addSubview:self.profileImage];
-                    UILabel* nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.backgroundView.frame.size.height-labelHeight, self.profileImage.frame.size.width, labelHeight)];
-                    nameLabel.textAlignment = NSTextAlignmentCenter;
-                    nameLabel.text = [NSString stringWithFormat:@"%@, %@", username, age];
-                    nameLabel.textColor = [UIColor whiteColor];
-                    nameLabel.backgroundColor = RED_COLOR;
-                    nameLabel.clipsToBounds = YES;
-                    nameLabel.layer.cornerRadius = cornRadius;
-                    [nameLabel setFont:[UIFont fontWithName:@"Helvetica" size:20]];
-                    UIFont *newFont = [UIFont fontWithName:[NSString stringWithFormat:@"%@-Bold",nameLabel.font.fontName] size:nameLabel.font.pointSize];
-                    [nameLabel setFont:newFont];
-                    [self.profileView addSubview:nameLabel];
+                    self.foregroundLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.backgroundView.frame.size.height-labelHeight, self.profileImage.frame.size.width, labelHeight)];
+                    self.foregroundLabel.textAlignment = NSTextAlignmentCenter;
+                    self.foregroundLabel.text = [NSString stringWithFormat:@"%@, %@", username, age];
+                    self.foregroundLabel.textColor = [UIColor whiteColor];
+                    self.foregroundLabel.backgroundColor = RED_COLOR;
+                    self.foregroundLabel.clipsToBounds = YES;
+                    self.foregroundLabel.layer.cornerRadius = cornRadius;
+                    [self.foregroundLabel setFont:[UIFont fontWithName:@"Helvetica" size:20]];
+                    UIFont *newFont = [UIFont fontWithName:[NSString stringWithFormat:@"%@-Bold",self.foregroundLabel.font.fontName] size:self.foregroundLabel.font.pointSize];
+                    if ([aUser[@"photo1"] isKindOfClass:[PFFile class]]) {
+                        PFFile* photo1 = aUser[@"photo1"];
+                        [photo1 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                            [self.arrayOfPhotoDataForeground addObject:data];
+                        }];
+                    }
+                    if ([aUser[@"photo2"] isKindOfClass:[PFFile class]]) {
+                        PFFile* photo2 = aUser[@"photo2"];
+                        [photo2 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                            [self.arrayOfPhotoDataForeground addObject:data];
+                        }];
+                    }
+                    if ([aUser[@"photo3"] isKindOfClass:[PFFile class]]) {
+                        PFFile* photo3 = aUser[@"photo3"];
+                        [photo3 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                            [self.arrayOfPhotoDataForeground addObject:data];
+                        }];
+                    }
+                    if ([aUser[@"photo4"] isKindOfClass:[PFFile class]]) {
+                        PFFile* photo4 = aUser[@"photo4"];
+                        [photo4 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                            [self.arrayOfPhotoDataForeground addObject:data];
+                        }];
+                    }
+                    [self.foregroundLabel setFont:newFont];
+                    [self.profileView addSubview:self.foregroundLabel];
                     [self setPanGestureRecognizer];
                     self.firstTime = NO;
 
@@ -129,7 +160,7 @@
     UserParse* aUser = self.posibleMatchesArray.firstObject;
     [self.posibleMatchesArray removeObject:aUser];
     self.backgroundUserProfile = aUser;
-
+    self.arrayOfPhotoDataBackground = [NSMutableArray new];
     PFFile* file = aUser[@"photo"];
     NSString* username = aUser[@"username"];
     NSLog(@"background user %@", aUser.username);
@@ -146,18 +177,42 @@
         [self.view addSubview:self.backgroundView];
         [self.backgroundView addSubview:self.backgroundImage];
         [self.view sendSubviewToBack:self.backgroundView];
-        UILabel* nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.backgroundView.frame.size.height-labelHeight, self.backgroundImage.frame.size.width, labelHeight)];
-        nameLabel.textAlignment = NSTextAlignmentCenter;
-        nameLabel.text = [NSString stringWithFormat:@"%@, %@", username, age];
-        nameLabel.textColor = [UIColor whiteColor];
-        nameLabel.backgroundColor = RED_COLOR;
-        nameLabel.clipsToBounds = YES;
-        nameLabel.layer.cornerRadius = cornRadius;
-        [nameLabel setFont:[UIFont fontWithName:@"Helvetica" size:20]];
-        UIFont *newFont = [UIFont fontWithName:[NSString stringWithFormat:@"%@-Bold",nameLabel.font.fontName] size:nameLabel.font.pointSize];
-        [nameLabel setFont:newFont];
-        [self.backgroundView addSubview:nameLabel];
+        self.backgroundLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.backgroundView.frame.size.height-labelHeight, self.backgroundImage.frame.size.width, labelHeight)];
+        self.backgroundLabel.textAlignment = NSTextAlignmentCenter;
+        self.backgroundLabel.text = [NSString stringWithFormat:@"%@, %@", username, age];
+        self.backgroundLabel.textColor = [UIColor whiteColor];
+        self.backgroundLabel.backgroundColor = RED_COLOR;
+        self.backgroundLabel.clipsToBounds = YES;
+        self.backgroundLabel.layer.cornerRadius = cornRadius;
+        [self.backgroundLabel setFont:[UIFont fontWithName:@"Helvetica" size:20]];
+        UIFont *newFont = [UIFont fontWithName:[NSString stringWithFormat:@"%@-Bold",self.backgroundLabel.font.fontName] size:self.backgroundLabel.font.pointSize];
+        [self.backgroundLabel setFont:newFont];
+        [self.backgroundView addSubview:self.backgroundLabel];
     }];
+    if ([aUser[@"photo1"] isKindOfClass:[PFFile class]]) {
+        PFFile* photo1 = aUser[@"photo1"];
+        [photo1 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            [self.arrayOfPhotoDataBackground addObject:data];
+        }];
+    }
+    if ([aUser[@"photo2"] isKindOfClass:[PFFile class]]) {
+        PFFile* photo2 = aUser[@"photo2"];
+        [photo2 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            [self.arrayOfPhotoDataBackground addObject:data];
+        }];
+    }
+    if ([aUser[@"photo3"] isKindOfClass:[PFFile class]]) {
+        PFFile* photo3 = aUser[@"photo3"];
+        [photo3 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            [self.arrayOfPhotoDataBackground addObject:data];
+        }];
+    }
+    if ([aUser[@"photo4"] isKindOfClass:[PFFile class]]) {
+        PFFile* photo4 = aUser[@"photo4"];
+        [photo4 getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            [self.arrayOfPhotoDataBackground addObject:data];
+        }];
+    }
 }
 
 - (CGRect)createMatchRect
@@ -197,32 +252,39 @@
     self.profileImage.layer.shadowRadius = 15.0;
     self.profileImage.layer.shadowOffset = (CGSize){0.0,20.0};
 
-    [UIView animateWithDuration:0.2 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut
+
+    [UIView animateWithDuration:0.4 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut
                      animations:^(void) {
                          self.profileImage.transform = CGAffineTransformMakeScale(-1, 1);
+                         self.foregroundLabel.transform = CGAffineTransformMakeScale(-1, -1);
                      }
                      completion:^(BOOL b) {
+                         self.foregroundLabel.transform = CGAffineTransformMakeScale(1, 1);
                          self.profileView.layer.shadowColor = [UIColor clearColor].CGColor;
                          self.profileView.layer.shadowOpacity = 0.0;
                          self.profileView.layer.shadowRadius = 0.0;
                          self.profileView.layer.shadowOffset = (CGSize){0.0, 0.0};
                          [self removeOldProfileImage];
+                         [self addNewProfileImage];
                      }];
 }
 
 - (void)addNewProfileImage
 {
-    PFFile* file = self.currShowingProfile[@"photo"];
-    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
-        [self.view addSubview:self.profileView];
-        self.profileImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.profileView.frame.size.width, self.profileView.frame.size.height-labelHeight)];
-        self.profileImage.tag = currentProfileImage;
-        self.profileImage.image = [UIImage imageWithData:data];
-        self.profileImage.clipsToBounds = YES;
-        self.profileImage.layer.cornerRadius = cornRadius;
-        [self.profileView addSubview:self.profileImage];
-    }];
-
+    NSData* data;
+    if (self.photoArrayIndex+1 == self.arrayOfPhotoDataForeground.count) {
+        data = [self.arrayOfPhotoDataForeground objectAtIndex:self.photoArrayIndex];
+        self.photoArrayIndex = 0;
+    } else {
+        data = [self.arrayOfPhotoDataForeground objectAtIndex:self.photoArrayIndex];
+        self.photoArrayIndex++;
+    }
+    self.profileImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.profileView.frame.size.width, self.profileView.frame.size.height-labelHeight)];
+    self.profileImage.tag = currentProfileImage;
+    self.profileImage.image = [UIImage imageWithData:data];
+    self.profileImage.clipsToBounds = YES;
+    self.profileImage.layer.cornerRadius = cornRadius;
+    [self.profileView addSubview:self.profileImage];
 }
 
 - (void)removeOldProfileImage
@@ -257,7 +319,7 @@
     else
     {
 //        self.profileView.transform = CGAffineTransformConcat(CGAffineTransformMakeTranslation(point.x, point.y), CGAffineTransformMakeRotation((M_PI_2)-1.45));
-        [UIView animateWithDuration:1 delay:0.5 options:UIViewAnimationOptionCurveLinear animations:^{
+        [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
             [self.profileView setTransform:CGAffineTransformConcat(CGAffineTransformMakeTranslation(point.x, point.y), CGAffineTransformMakeRotation(0.15))];
         }completion:^(BOOL finished){
             if (finished) {
@@ -276,7 +338,6 @@
         [UIView animateWithDuration:0.3 animations:^{
             self.profileView.transform = CGAffineTransformMakeTranslation(0, 0);
             self.profileView.alpha = 1;
-
         }];
         [self removeLikeAndDislikeView];
     }
@@ -292,7 +353,10 @@
         [self.profileView removeFromSuperview];
         self.profileView = self.backgroundView;
         self.profileImage = self.backgroundImage;
+        self.foregroundLabel = self.backgroundLabel;
+        self.arrayOfPhotoDataForeground = self.arrayOfPhotoDataBackground;
         self.profileImage.tag = currentProfileImage;
+        self.photoArrayIndex = 1;
         if ([self.willBeMatches containsObject:self.currShowingProfile]) {
             PFObject* match = [PFObject objectWithClassName:@"MessageParse"];
             match[@"fromUserParse"] = self.currShowingProfile;
@@ -337,7 +401,10 @@
         [self.profileView removeFromSuperview];
         self.profileView = self.backgroundView;
         self.profileImage = self.backgroundImage;
+        self.foregroundLabel = self.backgroundLabel;
         self.profileImage.tag = currentProfileImage;
+        self.photoArrayIndex = 1;
+        self.arrayOfPhotoDataForeground = self.arrayOfPhotoDataBackground;
         if ([self.willBeMatches containsObject:self.currShowingProfile]) {
             PFQuery* query = [PFQuery queryWithClassName:@"PossibleMatch"];
             [query whereKey:@"fromUser" equalTo:self.currShowingProfile];
@@ -376,7 +443,7 @@
 
 - (void) addDislikeView
 {
-    UIImageView* dislikeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.profileView.frame.size.width - (buttonWidth*2)+18, 0, buttonWidth, buttonHeight)];
+    UIImageView* dislikeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.profileImage.frame.size.width - buttonWidth, 0, buttonWidth, buttonHeight)];
     dislikeImageView.tag = dislikeViewTag;
     dislikeImageView.image = [UIImage imageNamed:@"dislike.png"];
     dislikeImageView.alpha = 0.01;
